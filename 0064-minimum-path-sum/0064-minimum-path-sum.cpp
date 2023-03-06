94be0940f1059b75b82e1c832ee0e54e040fb305
anne-ananya <124860809+anne-ananya@users.noreply.github.com>
2023-03-06 20:42:28 +0530

    Time: 12 ms (48.82%), Space: 10.1 MB (60.35%) - LeetHub

---
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& matrix) {
+        int n= matrix.size();
+        int m= matrix[0].size();
+         vector<vector<int> > dp(n,vector<int>(m,0));
+    for(int i=0; i<n ; i++){
+        for(int j=0; j<m; j++){
+            if(i==0 && j==0) dp[i][j] = matrix[i][j];
+            else{
+                
+                int up = matrix[i][j];
+                if(i>0) up += dp[i-1][j];
+                else up += 1e9;
+                
+                int left = matrix[i][j];
+                if(j>0) left+=dp[i][j-1];
+                else left += 1e9;
+                
+                dp[i][j] = min(up,left);
+            }
+        }
+    }
+    
+    return dp[n-1][m-1];
+    }
+};
+
