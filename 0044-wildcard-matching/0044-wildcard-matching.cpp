ac2c7471c44c1e648c3dd10f8cf3c0186352d800
anne-ananya <124860809+anne-ananya@users.noreply.github.com>
2023-03-17 15:05:18 +0530

    Time: 36 ms (91.22%), Space: 27.3 MB (37.55%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -2,18 +2,18 @@
 class Solution {
 public:
 	bool f(int i,int j,string& p,string& s,vector<vector<int>>& dp){
-		if(i<0 && j<0) return true;
-		if(i<0 && j>=0) return false;
-		if(j<0 && i>=0){
-			for(int k=i;k>=0;k--){
-				if(p[k]!='*') return false;
+		if(i==0 && j==0) return true;
+		if(i==0 && j>0) return false;
+		if(j==0 && i>0){
+			for(int k=i;k>=1;k--){
+				if(p[k-1]!='*') return false;
 			}
 			return true;
 		}
 		if(dp[i][j]!=-1) return dp[i][j];
 //      Match 
-		if(p[i]==s[j] || p[i]=='?') return dp[i][j]=f(i-1,j-1,p,s,dp);
-		if(p[i]=='*') return dp[i][j]=f(i-1,j,p,s,dp) || f(i,j-1,p,s,dp);
+		if(p[i-1]==s[j-1] || p[i-1]=='?') return dp[i][j]=f(i-1,j-1,p,s,dp);
+		if(p[i-1]=='*') return dp[i][j]=f(i-1,j,p,s,dp) || f(i,j-1,p,s,dp);
 //      NotMatch
 		return dp[i][j]=false;
 	}
@@ -21,7 +21,7 @@ public:
 	bool isMatch(string s, string p) {
 		int n=p.size();
 		int m=s.size();
-		vector<vector<int>> dp(n,vector<int>(m,-1));
-		return f(n-1,m-1,p,s,dp);
+		vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+		return f(n,m,p,s,dp);
 	}
 };
