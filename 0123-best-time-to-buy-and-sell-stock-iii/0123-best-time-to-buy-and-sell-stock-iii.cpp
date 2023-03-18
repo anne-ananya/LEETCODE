2332d5c5a7fa112e578a670ed42f266630069e0c
anne-ananya <124860809+anne-ananya@users.noreply.github.com>
2023-03-18 11:31:19 +0530

    Time: 229 ms (65.72%), Space: 75.4 MB (80.01%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,21 +1,23 @@
-
+//space optimized
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-       vector<vector<vector<int>>>dp(n+1, vector<vector<int>>(2, vector<int>(3,0)));
+       vector<vector<int>>after(2, vector<int>(3,0));
+        vector<vector<int>>cur(2, vector<int>(3,0));
         for(int i=n-1; i>=0; i--){
            for(int buy=0; buy<=1; buy++){
                for(int cap=1; cap<=2; cap++){ 
            if(buy==1){
-            dp[i][buy][cap] = max(dp[i+1][0][cap]-prices[i], dp[i+1][1][cap]);
+            cur[buy][cap] = max(after[0][cap]-prices[i], after[1][cap]);
         }
           else{
-             dp[i][buy][cap] = max(dp[i+1][1][cap-1]+prices[i], dp[i+1][0][cap] ); 
+             cur[buy][cap] = max(after[1][cap-1]+prices[i], after[0][cap] ); 
           }
         }
         }
+            after=cur;
         }
-        return dp[0][1][2];
+        return after[1][2];
     }
 };
