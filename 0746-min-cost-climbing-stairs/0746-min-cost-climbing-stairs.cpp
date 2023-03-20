3d170ebde823158b69f41e9150edd81a31f39871
anne-ananya <124860809+anne-ananya@users.noreply.github.com>
2023-03-21 00:40:41 +0530

    Time: 11 ms (30.44%), Space: 13.9 MB (34.47%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -1,18 +1,14 @@
-//memoization
+//tabulation
 class Solution {
 public:
-   int f(vector<int>& cost, int n, vector<int>& dp){
-       if(n<0)
-           return 0;
-       if(n==0 || n==1)
-           return dp[n]=cost[n];
-       if(dp[n]!=-1) return dp[n];
-       return dp[n]=cost[n]+ min(f(cost, n-1, dp),f(cost, n-2, dp));
-   }
     int minCostClimbingStairs(vector<int>& cost) {
         int n = cost.size();
-        vector<int>dp (n+1, -1);
-	return min(f(cost, n-1, dp),f(cost, n-2, dp));
+        vector<int>dp (n+1, 0);
+        for(int i=0; i<n; i++){
+           if (i<2) dp[i] = cost[i];
+		else dp[i] = cost[i] + min(dp[i-1], dp[i-2]);
+        }
+	return min(dp[n-1], dp[n-2]);
     }
 };
 
