0b8f0b21a3ec0aa434e07f2436f7a986d65fd7ff
anne-ananya <124860809+anne-ananya@users.noreply.github.com>
2023-03-22 11:00:54 +0530

    Time: 3 ms (92.60%), Space: 11.7 MB (67.94%) - LeetHub

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
@@ -4,10 +4,41 @@ public:
     vector<int> targetIndices(vector<int>& nums, int target) {
         sort(nums.begin(), nums.end());
         vector<int>ans;
-       for(int i=0; i<nums.size(); i++){
-           if(nums[i]==target)
-               ans.push_back(i);
-       }
+       int s=0, e= nums.size()-1;
+        int res1=-1;
+        //1st occurence
+        while(s<=e){
+            int mid= s+(e-s)/2;
+            if(target==nums[mid])
+            {
+                res1= mid;
+                e= mid-1;
+            }
+            else if(target<nums[mid])
+                e=mid-1;
+            else 
+                s=mid+1;
+        }
+        //2nd occurence
+         s=0;
+        e= nums.size()-1;
+        int res2=-1;
+         while(s<=e){
+             int mid= s+(e-s)/2;
+            if(target==nums[mid])
+            {
+                res2= mid;
+                s= mid+1;
+            }
+            else if(target<nums[mid])
+                e=mid-1;
+            else 
+                s=mid+1;
+        }
+        for(int i=res1; i<=res2; i++){
+            if(i!=-1)
+                ans.push_back(i);
+        }
         return ans;
     }
 };
