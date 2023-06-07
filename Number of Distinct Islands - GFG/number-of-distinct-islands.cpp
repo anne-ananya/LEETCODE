551362707c30b8d963b1164536a0d1a6d51e1e6d
anne-ananya <124860809+anne-ananya@users.noreply.github.com>
2023-06-07 18:43:12 +0530

    Added solution - LeetHub

---
+++ Number of Distinct Islands - GFG/number-of-distinct-islands.cpp
@@ -0,0 +1,83 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  private:
+    void dfs(int row, int col, vector < vector < int >> & vis,
+      vector < vector < int >> & grid, vector < pair < int, int >> & vec, int row0, 
+      int col0) {
+      // mark the cell as visited
+      vis[row][col] = 1;
+
+      // coordinates - base coordinates
+      vec.push_back({
+        row - row0,
+        col - col0
+      });
+      int n = grid.size();
+      int m = grid[0].size();
+
+      // delta row and delta column
+      int delrow[] = {-1, 0, +1, 0}; 
+      int delcol[] = {0, -1, 0, +1}; 
+
+      // traverse all 4 neighbours
+      for (int i = 0; i < 4; i++) {
+        int nrow = row + delrow[i];
+        int ncol = col + delcol[i];
+        // check for valid unvisited land neighbour coordinates 
+        if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m &&
+          !vis[nrow][ncol] && grid[nrow][ncol] == 1) {
+          dfs(nrow, ncol, vis, grid, vec, row0, col0);
+        }
+      }
+    }
+  public:
+    int countDistinctIslands(vector < vector < int >> & grid) {
+      int n = grid.size();
+      int m = grid[0].size();
+      vector < vector < int >> vis(n, vector < int > (m, 0));
+      set < vector < pair < int, int >>> st;
+
+      // traverse the grid
+      for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+          // if not visited and is a land cell
+          if (!vis[i][j] && grid[i][j] == 1) {
+            vector < pair < int, int >> vec;
+            dfs(i, j, vis, grid, vec, i, j);
+            // store in set
+            st.insert(vec);
+          }
+        }
+      }
+      return st.size();
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        vector<vector<int>> grid(n, vector<int>(m));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                cin >> grid[i][j];
+            }
+        }
+        Solution obj;
+        cout << obj.countDistinctIslands(grid) << endl;
+    }
+}
+// } Driver Code Ends
