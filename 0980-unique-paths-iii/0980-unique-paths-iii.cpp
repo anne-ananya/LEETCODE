80460522cf23cc8a6d537cadf985ad6e7fc28ddc
anne-ananya <124860809+anne-ananya@users.noreply.github.com>
2023-09-04 11:45:02 +0530

    Time: 4 ms (57.59%), Space: 7.1 MB (27.32%) - LeetHub

--- 0980-unique-paths-iii/0980-unique-paths-iii.cpp
+++ 0980-unique-paths-iii/0980-unique-paths-iii.cpp
@@ -1,43 +1,53 @@
 class Solution {
 public:
-  int ans = 0;
-    int nonObstacle = 1;
-   void dfs(int i,int j,vector<vector<int>>&grid,int count){
-    if(i<0 || i>=grid.size() || j<0 || j>=grid[0].size() ||grid[i][j] == -1){
-        return ;
+    int helper(int x1, int y1, int x2, int y2, int n, int m, vector<vector<int>>& grid, vector<vector<int>>& dp, int emptyCount) {
+        if (x1 < 0 || y1 < 0 || x1 >= n || y1 >= m || grid[x1][y1] == -1) {
+            return 0;
+        }
+        if (x1 == x2 && y1 == y2) {
+            return (emptyCount == 0) ? 1 : 0;
+        }
+        
+        int dr[] = {0, 1, 0, -1};
+        int dc[] = {1, 0, -1, 0};
+        int ans = 0;
+        grid[x1][y1]=-1;
+        for (int i = 0; i < 4; i++) {
+            int nrow = x1 + dr[i];
+            int ncol = y1 + dc[i];
+            ans += helper(nrow, ncol, x2, y2, n, m, grid, dp, emptyCount - 1);
+        }
+        grid[x1][y1]=0;
+        return dp[x1][y1] = ans;
     }
-    if(grid[i][j] == 2){
-
-       if(count == nonObstacle){
-            ans++;
-       }
-       return;
-    }
-
-    grid[i][j] = -1;
-
-    dfs(i+1,j,grid,count+1);
-    dfs(i-1,j,grid,count+1);
-    dfs(i,j+1,grid,count+1);
-    dfs(i,j-1,grid,count+1);
-
-    grid[i][j] = 0;
-   }
-
-
     int uniquePathsIII(vector<vector<int>>& grid) {
-
-        int startX = 0;
-        int startY = 0;
-        for(int i=0;i<grid.size();i++)
-        {
-            for(int j=0;j<grid[0].size();j++){
-                if(grid[i][j] == 1){startX = i; startY =j;}
-                else if(grid[i][j] == 0){nonObstacle++;}
+        int n = grid.size();
+        int m = grid[0].size();
+        int x1, y1, x2, y2;
+        
+        // Find the starting and ending squares
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 1) {
+                    x1 = i;
+                    y1 = j;
+                } else if (grid[i][j] == 2) {
+                    x2 = i;
+                    y2 = j;
+                }
             }
         }
-        dfs(startX,startY,grid,0);
-
-        return ans;
+        
+        int emptyCount = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 0) {
+                    emptyCount++;
+                }
+            }
+        }
+        
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+        return helper(x1, y1, x2, y2, n, m, grid, dp, emptyCount);
     }
 };
