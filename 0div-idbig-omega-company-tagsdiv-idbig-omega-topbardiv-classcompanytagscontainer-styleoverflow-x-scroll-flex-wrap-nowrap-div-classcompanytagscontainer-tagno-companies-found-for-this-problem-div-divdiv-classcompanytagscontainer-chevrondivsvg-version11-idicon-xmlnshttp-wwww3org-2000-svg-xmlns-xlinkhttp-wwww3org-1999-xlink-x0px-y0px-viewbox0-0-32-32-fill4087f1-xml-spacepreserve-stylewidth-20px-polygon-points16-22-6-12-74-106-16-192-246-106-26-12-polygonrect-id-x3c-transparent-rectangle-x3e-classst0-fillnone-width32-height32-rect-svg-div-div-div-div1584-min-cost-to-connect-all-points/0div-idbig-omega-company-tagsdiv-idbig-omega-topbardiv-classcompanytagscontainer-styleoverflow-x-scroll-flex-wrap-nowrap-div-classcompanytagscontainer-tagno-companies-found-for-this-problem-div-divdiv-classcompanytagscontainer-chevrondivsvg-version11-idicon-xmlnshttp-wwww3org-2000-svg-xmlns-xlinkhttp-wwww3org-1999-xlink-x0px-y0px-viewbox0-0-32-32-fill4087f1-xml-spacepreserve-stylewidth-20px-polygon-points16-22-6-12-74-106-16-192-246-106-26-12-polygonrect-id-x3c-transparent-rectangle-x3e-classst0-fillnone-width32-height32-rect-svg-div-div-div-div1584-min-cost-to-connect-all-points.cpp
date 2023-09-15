5ae4af80aa2d3ad7d1f0520c75a1e2c20aa41160
anne-ananya <124860809+anne-ananya@users.noreply.github.com>
2023-09-15 22:02:23 +0530

    Time: 236 ms (74.57%), Space: 14.7 MB (90.22%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1584-min-cost-to-connect-all-points/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1584-min-cost-to-connect-all-points.cpp
@@ -0,0 +1,32 @@
+class Solution{
+public:
+int minCostConnectPoints(vector<vector<int>>& points) {
+        int n=points.size();
+        int min_cost=0;
+        vector<bool> visited(n,false);
+        priority_queue<pair<int,int>, vector<pair<int,int>>, 
+        greater<pair<int,int>>> pq;
+        map<int,int> cache;
+        pq.push({0,0});
+        while(!pq.empty()){
+            auto edge=pq.top();
+            pq.pop();
+            int cost=edge.first;
+            int u=edge.second;
+            if(visited[u])continue;
+            visited[u]=true;
+            min_cost+=cost;
+            for(int v=0;v<n;v++){
+                if(!visited[v]){
+                    int dist=abs(points[u][0]-points[v][0])+
+                    abs(points[u][1]-points[v][1]);
+                    if(cache.find(v)==cache.end() || dist<cache[v]){
+                        cache[v]=dist;
+                        pq.push({dist,v});
+                    }
+                }
+            }
+        }
+        return min_cost;
+    }
+};
