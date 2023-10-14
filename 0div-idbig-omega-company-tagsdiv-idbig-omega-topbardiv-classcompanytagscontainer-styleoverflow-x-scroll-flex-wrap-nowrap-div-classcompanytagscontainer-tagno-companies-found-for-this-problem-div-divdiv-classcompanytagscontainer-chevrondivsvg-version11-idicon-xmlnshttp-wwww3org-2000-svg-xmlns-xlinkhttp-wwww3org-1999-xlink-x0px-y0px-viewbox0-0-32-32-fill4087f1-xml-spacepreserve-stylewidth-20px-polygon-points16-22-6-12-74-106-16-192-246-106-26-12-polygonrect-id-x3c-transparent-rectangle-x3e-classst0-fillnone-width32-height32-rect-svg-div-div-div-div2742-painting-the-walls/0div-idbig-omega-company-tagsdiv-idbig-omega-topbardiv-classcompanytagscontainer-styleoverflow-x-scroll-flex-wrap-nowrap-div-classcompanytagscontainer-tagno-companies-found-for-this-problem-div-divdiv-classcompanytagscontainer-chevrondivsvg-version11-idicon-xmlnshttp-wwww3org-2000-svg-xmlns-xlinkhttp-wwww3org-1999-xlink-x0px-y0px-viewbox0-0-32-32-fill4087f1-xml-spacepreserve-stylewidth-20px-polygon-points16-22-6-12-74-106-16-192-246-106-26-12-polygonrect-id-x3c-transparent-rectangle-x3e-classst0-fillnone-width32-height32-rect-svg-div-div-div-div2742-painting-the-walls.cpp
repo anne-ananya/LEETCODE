41eea1f96cf537602cdb5f6387bb3225988987b4
anne-ananya <124860809+anne-ananya@users.noreply.github.com>
2023-10-15 00:20:42 +0530

    Time: 195 ms (26.68%), Space: 115.8 MB (37.91%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2742-painting-the-walls/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2742-painting-the-walls.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2742-painting-the-walls/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2742-painting-the-walls.cpp
@@ -1,30 +1,21 @@
 class Solution {
 public:
-     int dp[501][501];
-     
-    long long find(int i,int topaint,vector<int>&cost,vector<int>&time){
-        if(topaint<=0)return 0;
-        if(dp[i][topaint]  != -1)return dp[i][topaint];
-
-        if(i == 0){
-            long long cur = topaint - (time[i]+1);
-            if(cur>0)return dp[i][topaint]= INT_MAX;
-            else return dp[i][topaint] = cost[i];
-        }
-        else{
-    
-            long long cur = cost[i] + find(i-1,(topaint-(time[i]+1)),cost,time);
-            cur = min(cur,find(i-1,topaint,cost,time));
-            return dp[i][topaint] = cur;
-        }
+    int f(int ind, int rem, vector<int>& cost, vector<int>& time, vector<vector<int>>& dp) {
+        if (rem <= 0)
+            return 0;
+        if (ind <0)
+            return 1e9;
+        if (dp[ind][rem] != -1)
+            return dp[ind][rem];
+        int paint = cost[ind] + f(ind - 1, rem - 1 - time[ind], cost, time, dp);
+        int not_paint = f(ind-1, rem, cost, time, dp);
+        dp[ind][rem] = min(paint, not_paint);
+        return dp[ind][rem];
     }
 
     int paintWalls(vector<int>& cost, vector<int>& time) {
-        
-        int ans = 0;
-        memset(dp,-1,sizeof(dp));
         int n = cost.size();
-        return find(n-1,n,cost,time);
+        vector<vector<int>> dp(n, vector<int>(n + 1, -1));
+        return f(n-1, n, cost, time, dp);
     }
-  
 };
