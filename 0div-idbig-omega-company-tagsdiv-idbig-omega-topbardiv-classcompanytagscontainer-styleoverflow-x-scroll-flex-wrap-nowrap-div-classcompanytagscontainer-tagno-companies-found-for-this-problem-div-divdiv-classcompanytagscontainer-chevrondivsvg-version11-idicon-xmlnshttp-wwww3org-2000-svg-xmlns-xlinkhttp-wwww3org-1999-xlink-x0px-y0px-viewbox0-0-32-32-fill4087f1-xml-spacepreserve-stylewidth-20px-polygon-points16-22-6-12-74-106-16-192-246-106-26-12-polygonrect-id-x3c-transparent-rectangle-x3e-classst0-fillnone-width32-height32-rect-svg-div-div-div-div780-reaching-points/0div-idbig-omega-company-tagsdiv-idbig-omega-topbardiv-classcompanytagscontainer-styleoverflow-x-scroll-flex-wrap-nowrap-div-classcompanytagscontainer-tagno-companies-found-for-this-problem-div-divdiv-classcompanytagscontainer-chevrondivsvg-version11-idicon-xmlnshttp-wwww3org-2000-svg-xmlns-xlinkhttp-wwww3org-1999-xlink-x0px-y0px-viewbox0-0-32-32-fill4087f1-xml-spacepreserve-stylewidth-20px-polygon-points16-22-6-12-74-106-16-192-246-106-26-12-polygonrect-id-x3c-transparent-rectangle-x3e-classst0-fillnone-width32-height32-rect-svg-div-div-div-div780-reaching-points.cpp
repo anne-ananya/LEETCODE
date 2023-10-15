392fab4701d6206b2e422d63eea4708802a3b066
anne-ananya <124860809+anne-ananya@users.noreply.github.com>
2023-10-15 23:32:21 +0530

    Time: 253 ms (5.06%), Space: 6.4 MB (15.47%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div780-reaching-points/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div780-reaching-points.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+	bool reachingPoints(int sx, int sy, int tx, int ty) {
+		if(tx<sx || ty<sy) return false;
+		if(tx==sx)
+			return (ty-sy)%sx==0;
+		if(ty==sy)
+			return (tx-sx)%sy==0;
+		if(tx>ty)
+			return reachingPoints(sx,sy,tx-ty,ty);
+		else
+			return reachingPoints(sx,sy,tx,ty-tx);
+	}
+};
